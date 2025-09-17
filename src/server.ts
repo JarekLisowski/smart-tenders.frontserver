54629bd6f312b5989e7b5ad62f7a5a45f2183286
Jarosław Lisowski <jl@macrix.pl>
2025-09-17 18:32:45 +0200

    fix SPA configuration

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(express.static(publicPath));
 // Optional:  A catch-all route for any request that doesn't match a static file.
 // This is useful for single-page applications (SPAs) where you want the client-side
 // JavaScript router to handle navigation.  Remove this if you don't need it.
- app.get('*', (req: Request, res: Response) => {
+ app.get('/*path', (req: Request, res: Response) => {
      res.sendFile(path.join(__dirname, '../public', 'index.html'));
  });
 

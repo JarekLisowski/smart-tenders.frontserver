ecf16e839723d80d49a7c7c9f503711f37d4a4ff
Jarosław Lisowski <jl@macrix.pl>
2025-09-17 16:46:21 +0200

    enable Single Page App functionality

--- src/server.ts
+++ src/server.ts
@@ -14,9 +14,9 @@ app.use(express.static(publicPath));
 // Optional:  A catch-all route for any request that doesn't match a static file.
 // This is useful for single-page applications (SPAs) where you want the client-side
 // JavaScript router to handle navigation.  Remove this if you don't need it.
-// app.get('*', (req: Request, res: Response) => {
-//     res.sendFile(path.join(__dirname, '../public', 'index.html'));
-// });
+ app.get('*', (req: Request, res: Response) => {
+     res.sendFile(path.join(__dirname, '../public', 'index.html'));
+ });
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
